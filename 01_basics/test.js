eed12b2ff4e6fc958fed97552dce0addd8a45418
rajesh8239694593 <168459561+rajesh8239694593@users.noreply.github.com>
2024-11-23 19:08:54 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,28 @@
+// 1.what is going on afrer write console.log("hello");
+
+console.log("hello");
+
+// 2.how to set up enviroment variabel and why we set up enviroment virable??
+// Ans.  to run your code 
+
+// 3. how many enviroment  to javascript
+// Ans 1.node js
+    // 2.Deno js
+
+// 4. commend for see node version and run file
+// node -v
+// node test.js
+
+// 5. how to setup your project on git hub
+//  create prpository
+// public
+// code space
+// add dev container config file
+// show all definitaion
+// select node js and java script
+
+// 6.how to save github files
+// container config
+// commit
+// push
+// 
