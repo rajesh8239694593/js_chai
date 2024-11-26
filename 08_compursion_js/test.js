8468804fd48d4cd36feb676fb3fe6acbf52b8abb
rajesh8239694593 <168459561+rajesh8239694593@users.noreply.github.com>
2024-11-26 18:20:37 +0000

    com

---
+++ 08_compursion_js/test.js
@@ -0,0 +1,42 @@
+// 1.Answer these questions on compersion operator
+
+// console.log( 2 > 1 )
+// console.log( 2 < 1 )
+// console.log( 2 >= 1 )
+// console.log( 2 <= 1 )
+// console.log( 2 != 1 )
+// console.log( 2 > 1 )
+
+
+// 2.surprising answere these questions
+
+
+// console.log("2" > 1 )             //true 
+
+// console.log("02" > 1 )             //true 
+
+// console.log(null > 0 )             //false because null convert into 0
+// console.log(null == 0 )             //false
+// console.log(null >= 0 )             //true 
+
+// console.log(undefined == 0 )             //false 
+
+// console.log(undefined < 0 )             //false 
+
+
+// console.log(undefined > 0 )             //false 
+
+
+
+// 3. == and === check
+
+console.log("2" == 2)                     //true
+console.log("2" === 2)                     //false
+
+
+
+
+
+// 4. *******************note*******************
+// avoid these type of confusion compersion
+// use clean code only
